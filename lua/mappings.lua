991309a5685e2d4412b5f9a2e942798fada99deb
dodofarm <ea01052002@gmail.com>
2024-12-16 13:47:34 +0000

    Added shortcut to copy linter messages <leader>y

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -15,4 +15,15 @@ map("n", "<leader>dsi", "<cmd> DapStepIn <CR>", { desc = "DAP Step in" })
 map("n", "<leader>dc", "<cmd> DapContinue <CR>", { desc = "DAP Continue" })
 map("n", "<leader>dt", "<cmd> DapTerminate <CR>", { desc = "DAP Terminate" })
 
+map("n", "<leader>y", [[:lua YankDiagnosticError()<CR>]], { noremap = true, silent = true, desc = "Copy error" })
+
+function YankDiagnosticError()
+  vim.diagnostic.open_float()
+  vim.diagnostic.open_float()
+  local win_id = vim.fn.win_getid() -- get the window ID of the floating window
+  vim.cmd "normal! j" -- move down one row
+  vim.cmd "normal! VG" -- select everything from that row down
+  vim.cmd "normal! y" -- yank selected text
+  vim.api.nvim_win_close(win_id, true) -- close the floating window by its ID
+end
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
